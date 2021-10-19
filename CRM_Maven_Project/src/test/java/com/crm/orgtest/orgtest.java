3db01456ea9b7ed43fedb12c2cbc857022cbaff5
Neha Meshram <Neha Meshram@LAPTOP-QS2J1VMC>
2021-10-19 14:21:34 +0530

    push

---
+++ CRM_Maven_Project/src/test/java/com/crm/orgtest/orgtest.java
@@ -0,0 +1,19 @@
+package com.crm.orgtest;
+
+import org.testng.annotations.Test;
+
+public class orgtest 
+{
+	@Test
+	public void createorgtest()
+	{
+	System.out.println("execute orgtest");
+	}
+	
+	@Test
+	public void edit()
+	{
+	System.out.println("edit org");
+	}
+
+}
